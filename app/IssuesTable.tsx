fd34d3cf97d5b9ef395bf6b0b01c26a21ce95f1e
Levis155 <levismbui@gmail.com>
2025-10-28 07:27:05 +0300

    First commit

---
+++ app/IssuesTable.tsx
@@ -0,0 +1,56 @@
+import { Table, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import IssueStatusBadge from "./IssueStatusBadge";
+import { Issue } from "./generated/prisma/client";
+import prisma from "@/prisma/client";
+
+const IssuesTable = async () => {
+  const issues: Issue[] = await prisma.issue.findMany();
+
+  return (
+    <Table.Root variant="surface" mt="5">
+      <Table.Header>
+        <Table.Row>
+          {columns.map((column) => (
+            <Table.ColumnHeaderCell
+              key={column.value}
+              className={column.className}
+            >
+              <Link href={""}>{column.label}</Link>
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map((issue) => (
+          <Table.Row key={issue.id}>
+            <Table.Cell>
+              <Text>{issue.title}</Text>
+              <div className="block md:hidden">
+                <IssueStatusBadge status={issue.status} />
+              </div>
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              <IssueStatusBadge status={issue.status} />
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              {issue.createdAt.toDateString()}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
+
+const columns: {
+  label: string;
+  value: keyof Issue & string;
+  className?: string;
+}[] = [
+  { label: "Issue", value: "title" },
+  { label: "Status", value: "status", className: "hidden md:table-cell" },
+  { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+];
+
+export default IssuesTable;
